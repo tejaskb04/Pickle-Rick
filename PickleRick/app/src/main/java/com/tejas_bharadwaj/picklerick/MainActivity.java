daa1ff222806479e6240a527a0b3e86b1c9a66b0
tejaskb04 <tejaskb@gmail.com>
2017-09-26 09:18:14 -0700

    Added Pickle Rick

--- PickleRick/app/src/main/java/com/tejas_bharadwaj/picklerick/MainActivity.java
+++ PickleRick/app/src/main/java/com/tejas_bharadwaj/picklerick/MainActivity.java
@@ -2,12 +2,23 @@ package com.tejas_bharadwaj.picklerick;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button pickleRickBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pickleRickBtn = (Button) findViewById(R.id.pickle_rick_button);
+        pickleRickBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
